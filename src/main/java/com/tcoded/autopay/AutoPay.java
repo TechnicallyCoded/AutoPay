d77cbfda39d69a1674d26a0c5a1e94c615230f04
TechnicallyCoded <technicallycoded@gmail.com>
2023-04-10 22:29:16 +0200

    Folia support

--- src/main/java/com/tcoded/autopay/AutoPay.java
+++ src/main/java/com/tcoded/autopay/AutoPay.java
@@ -1,14 +1,20 @@
 package com.tcoded.autopay;
 
 import com.tcoded.autopay.util.VaultHook;
+import com.tcoded.folialib.FoliaLib;
+import com.tcoded.folialib.impl.ServerImplementation;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.concurrent.TimeUnit;
 
 public final class AutoPay extends JavaPlugin {
 
     private VaultHook vaultHook;
     
     private double payAmount;
+    private ServerImplementation schedulerImpl;
+    private FoliaLib foliaLib;
 
     @Override
     public void onEnable() {
@@ -20,13 +26,17 @@ public final class AutoPay extends JavaPlugin {
         int interval = getConfig().getInt("payment-interval", 60 * 20); // 1 per minute default
         payAmount = getConfig().getDouble("payment-amount", 1.0d);
 
-        this.getServer().getScheduler().runTaskTimer(this, this::autoPay, 20, interval);
+        this.foliaLib = new FoliaLib(this);
+        this.schedulerImpl = foliaLib.getImpl();
+        this.schedulerImpl.runTimer(this::autoPay, 20 * 50, interval * 50L, TimeUnit.MILLISECONDS);
     }
 
     private void autoPay() {
-        for (Player player : this.getServer().getOnlinePlayers()) {
-            this.vaultHook.addMoney(player, payAmount);
-        }
+        for (Player player : this.getServer().getOnlinePlayers()) addMoney(player);
+    }
+
+    private void addMoney(Player player) {
+        this.vaultHook.addMoney(player, payAmount);
     }
 
     @Override
